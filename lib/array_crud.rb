2f848718fc3792c19e1d849d1aa474a98fa77658
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-14 18:34:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  bros = ["Ryan", "Cody", "Mark"]
+  bros = ["Ryan", "Cody", "Mark", "Stephen"]
 end
 
 def add_element_to_end_of_array(array, element)
-  bros.push("Stephen")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
